e6cc252a8cf1c31877d9c326b3e76aa1c7830cf7
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2022-04-30 22:01:45 +0200

    update 4

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-include_once 'Classes\DVD.php';
-include_once 'Classes\Product.php';
+include_once 'Classes'.str_starts_with(strtoupper(PHP_OS),"WIN") ? "\\" :"/".'CD.php';
+include_once 'Classes'.str_starts_with(strtoupper(PHP_OS),"WIN") ? "\\" :"/".'DVD.php';
+include_once 'Classes'.str_starts_with(strtoupper(PHP_OS),"WIN") ? "\\" :"/".'Product.php';
 $dvd = new DVD();
 $dvd->setLengteInMinuten(107);
 $dvd->setNaam('The Shawshank Redemption');
